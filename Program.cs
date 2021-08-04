1e668a0e4f095e9379e7f4db04252cbfd3eadcd2
UpHash <yukawallstudio@gmail.com>
2021-08-05 04:13:32 +0900

    リポジトリ作成

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MakiOneDrawingBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
