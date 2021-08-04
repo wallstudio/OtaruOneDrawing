6672a88dd773aa47583c53d46e314aecc364d522
UpHash <yukawallstudio@gmail.com>
2021-08-05 06:42:01 +0900

    引数をダンプ

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MakiOneDrawingBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Hello World! {string.Join(",", args)}");
         }
     }
 }
