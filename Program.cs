e504194345ecdd215c3227191b5c1a70f28eb20b
UpHash <yukawallstudio@gmail.com>
2021-08-06 00:02:43 +0900

    各Jobを作成

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Linq;
 
 namespace MakiOneDrawingBot
 {
@@ -7,7 +8,6 @@ namespace MakiOneDrawingBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             foreach (var env in args)
             {
                 Console.WriteLine($"arg:{env}");
@@ -16,6 +16,33 @@ namespace MakiOneDrawingBot
             {
                 Console.WriteLine($"{env.Key}={env.Value}");
             }
+
+            var command = args.SkipWhile(a => a != "--command").Skip(1).FirstOrDefault() ?? throw new ArgumentException("--command");
+            var twitterApiKey = args.SkipWhile(a => a != "--twitter-api-key").Skip(1).FirstOrDefault() ?? throw new ArgumentException("--twitter-api-key");
+            var twitterApiSecret = args.SkipWhile(a => a != "--twitter-api-secret").Skip(1).FirstOrDefault() ?? throw new ArgumentException("--twitter-api-secret");
+
+            switch (command)
+            {
+                case "NotificationStart":
+                    NotificationStart();
+                    return;
+                case "AccumulationPosts":
+                    AccumulationPosts();
+                    return;
+                case "NotificationFinish":
+                    NotificationFinish();
+                    return;
+                case "NotificationMorning":
+                    NotificationMorning();
+                    return;
+                default:
+                    throw new ArgumentException($"--command={command}");
+            }
         }
+
+        static void NotificationStart() => Console.WriteLine(nameof(NotificationStart));
+        static void AccumulationPosts() => Console.WriteLine(nameof(AccumulationPosts));
+        static void NotificationFinish() => Console.WriteLine(nameof(NotificationFinish));
+        static void NotificationMorning() => Console.WriteLine(nameof(NotificationMorning));
     }
 }
