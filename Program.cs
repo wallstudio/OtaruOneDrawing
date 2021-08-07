4dc69306e3d8c9becdde2960127cfc92d2c03a75
UpHash <yukawallstudio@gmail.com>
2021-08-07 10:02:12 +0900

    CoreTweet追加

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 
 namespace MakiOneDrawingBot
 {
+    // https://www.slideshare.net/ngzm/oauth-10-oauth-20-openid-connect
+
     class Program
     {
         static void Main(string[] args)
@@ -20,9 +24,16 @@ namespace MakiOneDrawingBot
             var command = args.SkipWhile(a => a != "--command").Skip(1).FirstOrDefault() ?? throw new ArgumentException("--command");
             var twitterApiKey = args.SkipWhile(a => a != "--twitter-api-key").Skip(1).FirstOrDefault() ?? throw new ArgumentException("--twitter-api-key");
             var twitterApiSecret = args.SkipWhile(a => a != "--twitter-api-secret").Skip(1).FirstOrDefault() ?? throw new ArgumentException("--twitter-api-secret");
+            var bearerToken = args.SkipWhile(a => a != "--bearer-token").Skip(1).FirstOrDefault() ?? throw new ArgumentException("--bearer-token");
+            var accessToken = args.SkipWhile(a => a != "--access-token").Skip(1).FirstOrDefault() ?? throw new ArgumentException("--access-token");
+            var accessTokenSecret = args.SkipWhile(a => a != "--access-token-secret").Skip(1).FirstOrDefault() ?? throw new ArgumentException("--access-token-secret");
 
             switch (command)
             {
+                case "Interactive":
+                    var newCommand = Console.ReadLine();
+                    Main(args.Select(a => a == command ? newCommand : a).ToArray());
+                    return;
                 case "NotificationStart":
                     NotificationStart();
                     return;
@@ -40,9 +51,24 @@ namespace MakiOneDrawingBot
             }
         }
 
-        static void NotificationStart() => Console.WriteLine(nameof(NotificationStart));
-        static void AccumulationPosts() => Console.WriteLine(nameof(AccumulationPosts));
-        static void NotificationFinish() => Console.WriteLine(nameof(NotificationFinish));
+        /// <summary>
+        /// 朝の予告ツイートを投げる
+        /// </summary>
         static void NotificationMorning() => Console.WriteLine(nameof(NotificationMorning));
+
+        /// <summary>
+        /// ワンドロ開始のツイートを投げる
+        /// </summary>
+        static void NotificationStart() => Console.WriteLine(nameof(NotificationStart));
+
+        /// <summary>
+        /// ワンドロ終了のツイートを投げる
+        /// </summary>
+        static void NotificationFinish() => Console.WriteLine(nameof(NotificationFinish));
+
+        /// <summary>
+        /// 投稿を集計してRTとランキングを更新する
+        /// </summary>
+        static void AccumulationPosts() => Console.WriteLine(nameof(AccumulationPosts));
     }
 }
