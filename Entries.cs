39fc1996ba92ecb1dd2be498a18a6914752afbc7
UpHash <yukawallstudio@gmail.com>
2022-01-13 07:07:11 +0900

    GAS カレンダーベースを廃止してシンプルに

--- Entries.cs
+++ Entries.cs
@@ -1,11 +1,16 @@
+using System;
 using System.Collections.Generic;
 
 namespace MakiOneDrawingBot.Entries;
 
 
-[TableName("schedule2")]
+[TableName("schedule3")]
 public class Schedule : EntryBase
 {
+    [Column] public TimeOnly? PreTime { get; set; }
+    [Column] public TimeOnly? BeginTime { get; set; }
+    [Column] public TimeOnly? PostTime { get; set; }
+    [Column] public TimeOnly? AccTime { get; set; }
     [Column] public string Theme1 { get; set; }
     [Column] public string Theme2 { get; set; }
     [Column] public long? PreId { get; set; }
@@ -16,6 +21,10 @@ public class Schedule : EntryBase
     public override void Deserialize(Dictionary<string, string> columns)
     {
         base.Deserialize(columns);
+        PreTime = TimeOnly.TryParse(columns[nameof(PreTime)], out var preTime) ? preTime : null;
+        BeginTime = TimeOnly.TryParse(columns[nameof(BeginTime)], out var beginTime) ? beginTime : null;
+        PostTime = TimeOnly.TryParse(columns[nameof(PostTime)], out var endTime) ? endTime : null;
+        AccTime = TimeOnly.TryParse(columns[nameof(AccTime)], out var accTime) ? accTime : null;
         PreId = long.TryParse(columns[nameof(PreId)], out var preId) ? preId : null;
         BeginId = long.TryParse(columns[nameof(BeginId)], out var beginId) ? beginId : null;
         EndId = long.TryParse(columns[nameof(EndId)], out var endId) ? endId : null;
--- gas/Scheduler.ts
+++ gas/Scheduler.ts
@@ -1,132 +1,60 @@
-const CALENDER_ID = "22326mbg1cbd88bi5s7e9cklbg@group.calendar.google.com";
-const EVENT_NAME = "弦巻マキ深夜の真剣お絵描き60分勝負";
+var isDryMode = false;
 
 function onTrigger(arg: GoogleAppsScript.Events.AppsScriptEvent)
 {
-	if(arg == undefined)
+	isDryMode = arg == undefined; // need launch from trigger
+	console.log(`triggered ${arg?.triggerUid ?? "???"}`);
+
+	const span = 5 * 60 * 1000;
+	const now = new Date();
+	const schedules = new ScheduleTable("1Un15MnW9Z2ChwSdsxdAVw495uSmJN4jBHngcBpYxo_0");
+
+	let taskCount = 0;
+	const commands: { id: string, cmd: string, timing: Date }[] = [];
+	for (const schedule of schedules.entries)
 	{
-		console.log("need launch from trigger");
-		return;
+		if(schedule.pre.getTime()-span < now.getTime() && now.getTime() < schedule.pre.getTime())
+			commands.push({ id: schedule.id, cmd: "NotificationMorning", timing: schedule.pre });
+		else if(schedule.begin.getTime()-span < now.getTime() && now.getTime() < schedule.begin.getTime())
+			commands.push({ id: schedule.id, cmd: "NotificationStart", timing: schedule.begin });
+		else if(schedule.end.getTime()-span < now.getTime() && now.getTime() < schedule.end.getTime())
+			commands.push({ id: schedule.id, cmd: "NotificationFinish", timing: schedule.end });
+		else if(schedule.acc.getTime()-span < now.getTime() && now.getTime() < schedule.acc.getTime())
+			commands.push({ id: schedule.id, cmd: "AccumulationPosts", timing: schedule.acc });
 	}
-	else
+	for (const command of commands)
 	{
-		console.log(`triggered ${arg.triggerUid}`);
+		run(command.id, command.cmd, command.timing);
 	}
 
-	const date = new Date();
-	const span = 5;
-	const searchStart = new Date(date.getFullYear(), date.getMonth() - 1, date.getDate());
-	const searchEnd = new Date(date.getFullYear(), date.getMonth() + 1, date.getDate());
-	const events = CalendarApp.getCalendarById(CALENDER_ID)
-		.getEvents(searchStart, searchEnd)
-		.filter(ev => ev.getTitle() == EVENT_NAME);
-
-	const previousEvent = events.reverse().find(i => toDateTick(i.getStartTime()) < toDateTick(date));
-	const nextEvent = events.find(i => toDateTick(i.getEndTime()) > toDateTick(date));
-	const toDayEvent = events.find(i => toDateTick(i.getStartTime()) == toDateTick(date) || toDateTick(i.getEndTime()) == toDateTick(date));
-	const runningEvent = events.find(i => approximately(i.getStartTime(), span, date) >= 0  && approximately(i.getEndTime(), span, date) <= 0);
-	console.log(`previousEvent: ${previousEvent?.getTitle()} ${previousEvent?.getStartTime()}-${previousEvent?.getEndTime()}`);
-	console.log(`nextEvent: ${nextEvent?.getTitle()} ${nextEvent?.getStartTime()}-${nextEvent?.getEndTime()}`);
-	console.log(`toDayEvent: ${toDayEvent?.getTitle()} ${toDayEvent?.getStartTime()}-${toDayEvent?.getEndTime()}`);
-	console.log(`runningEvent: ${runningEvent?.getTitle()} ${runningEvent?.getStartTime()}-${runningEvent?.getEndTime()}`);
-
-	const morningTime = new Date(toDayEvent?.getStartTime()?.getFullYear(), toDayEvent?.getStartTime()?.getMonth(), toDayEvent?.getStartTime()?.getDate(), 9, 30);
-	const accumulateTime = new Date(previousEvent?.getStartTime()?.getFullYear(), previousEvent?.getStartTime()?.getMonth(), previousEvent?.getStartTime()?.getDate(), 36, 45);
-	if(toDayEvent != null
-		&& 0 == approximately(morningTime, span, date))
-	{
-		run("NotificationMorning", morningTime, toDayEvent.getStartTime(), nextEvent?.getStartTime());
-	}
-	else if(runningEvent != null
-		&& 0 == approximately(runningEvent.getStartTime(), span, date))
-	{
-		run("NotificationStart", runningEvent.getStartTime(), runningEvent.getStartTime(), nextEvent?.getStartTime());
-	}
-	else if(runningEvent != null
-		&& 0 == approximately(runningEvent.getEndTime(), span, date))
-	{
-		run("NotificationFinish", runningEvent.getEndTime(), runningEvent.getStartTime(), nextEvent?.getStartTime());
-	}
-	else if(previousEvent?.getStartTime()?.getDate() == date.getDate() - 1
-		&& 0 == approximately(accumulateTime, span, date))
-	{
-		run("AccumulationPosts", accumulateTime, previousEvent.getStartTime(), nextEvent?.getStartTime());
-	}
-	else
-	{
-		console.log("no task");
-	}
+	console.log(`finish ${commands.length}`);
 }
 
-function run(workflowName : string, actionDate : Date | GoogleAppsScript.Base.Date, eventDate : Date | GoogleAppsScript.Base.Date, nextDate : Date | GoogleAppsScript.Base.Date) : void
+function run(eventId: string, workflowName: string, timing: Date) : void
 {
-	console.log(`workflow:${workflowName}, actionDate: ${actionDate}, eventDate:${eventDate}, nextDate:${nextDate}`);
+	console.log(`id:${eventId}, workflowName:${workflowName}, timing:${timing.toString()}`);
 	const headers = {
-			Authorization : `token ${getGithubToken()}`,
-			Accept : "application/vnd.github.v3+json"
+		Authorization : `token ${getGithubToken()}`,
+		Accept : "application/vnd.github.v3+json"
 	};
 	const workflowsResponse = UrlFetchApp.fetch(`https://api.github.com/repos/wallstudio/MakiOneDrawing/actions/workflows`, { method: "get", headers: headers });
 	const workflows : any[] = JSON.parse(workflowsResponse.getContentText()).workflows;
 	const workflow = workflows.find(w => w.name == "Main");
 
-	let res = UrlFetchApp.fetch(`https://api.github.com/repos/wallstudio/MakiOneDrawing/actions/workflows/${workflow.id}/dispatches`,
-	{
-		method: "post",
-		headers: headers,
-		payload: JSON.stringify({ ref: "master", inputs:
-		{
+	const payload = JSON.stringify({
+		ref: "master",
+		inputs: {
+			eventDate: eventId,
 			command: workflowName,
-			actionDate : `${actionDate.getFullYear()}/${actionDate.getMonth() + 1}/${actionDate.getDate()} ${actionDate.getHours()}:${actionDate.getMinutes()} +09:00`,
-			eventDate: `${eventDate.getFullYear()}/${eventDate.getMonth() + 1}/${eventDate.getDate()} ${eventDate.getHours()}:${eventDate.getMinutes()} +09:00`,
-			nextDate: `${nextDate?.getFullYear()}/${nextDate?.getMonth() + 1}/${nextDate?.getDate()} ${nextDate?.getHours()}:${nextDate?.getMinutes()} +09:00`,
+			actionDate : `${timing.getFullYear()}/${timing.getMonth() + 1}/${timing.getDate()} ${timing.getHours()}:${timing.getMinutes()} +09:00`,
 			general: "From GAS trigger."
-		}})
-	});
+	}});
+	console.log(`${payload}`);
+	
+	if(isDryMode) return;
+
+	let res = UrlFetchApp.fetch(
+		`https://api.github.com/repos/wallstudio/MakiOneDrawing/actions/workflows/${workflow.id}/dispatches`,
+		{ method: "post", headers: headers, payload: payload});
 	console.log(`${res.getResponseCode()}`);
-}
-
-function approximately(ref : Date | GoogleAppsScript.Base.Date, span : number, date : Date) : number
-{
-	const dateStart = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes());
-	const dateEnd = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes() + span);
-	if(dateStart <= ref && ref < dateEnd)
-	{
-		return 0;
-	}
-	else if (ref < dateStart)
-	{
-		return +1;
-	}
-	else if (dateEnd <= ref)
-	{
-		return -1;
-	}
-}
-
-function makeSchedules()
-{
-	const description = "詳細はこちら！\nhttps://wallstudio.github.io/MakiOneDrawing/";
-	const date = new Date();
-	for (
-		let d = date;
-		d.getTime() < new Date(date.getFullYear(), date.getMonth() + 2).getTime(); // とりあえず来月末まで
-		d = new Date(d.getFullYear(), d.getMonth(), d.getDate() + 1))
-	{
-		if(d.getDate() % 10 != 3) continue;
-
-		const start = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 22);
-		const end = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 25);
-		CalendarApp.getCalendarById(CALENDER_ID).createEvent(EVENT_NAME, start, end, { "description" : description});
-		console.log(`Scheduled at ${start.toLocaleString()}`);
-	}
-}
-
-function toDate(date : Date | GoogleAppsScript.Base.Date) : Date
-{
-	return new Date(date.toLocaleDateString());
-}
-
-function toDateTick(date : Date | GoogleAppsScript.Base.Date) : number
-{
-	return toDate(date).getTime();
 }
