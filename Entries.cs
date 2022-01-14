e8e82b5a5d37ffaa08eda6fda6515a2470fec514
UpHash <yukawallstudio@gmail.com>
2022-01-15 06:46:55 +0900

    24時を超える場合のパースを修正

--- Entries.cs
+++ Entries.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace MakiOneDrawingBot.Entries;
 
@@ -7,10 +8,10 @@ namespace MakiOneDrawingBot.Entries;
 [TableName("schedule3")]
 public class Schedule : EntryBase
 {
-    [Column] public TimeOnly? PreTime { get; set; }
-    [Column] public TimeOnly? BeginTime { get; set; }
-    [Column] public TimeOnly? EndTime { get; set; }
-    [Column] public TimeOnly? AccTime { get; set; }
+    [Column] public TimeSpan? PreTime { get; set; }
+    [Column] public TimeSpan? BeginTime { get; set; }
+    [Column] public TimeSpan? EndTime { get; set; }
+    [Column] public TimeSpan? AccTime { get; set; }
     [Column] public string Theme1 { get; set; }
     [Column] public string Theme2 { get; set; }
     [Column] public long? PreId { get; set; }
@@ -21,14 +22,28 @@ public class Schedule : EntryBase
     public override void Deserialize(Dictionary<string, string> columns)
     {
         base.Deserialize(columns);
-        PreTime = TimeOnly.TryParse(columns[nameof(PreTime)], out var preTime) ? preTime : null;
-        BeginTime = TimeOnly.TryParse(columns[nameof(BeginTime)], out var beginTime) ? beginTime : null;
-        EndTime = TimeOnly.TryParse(columns[nameof(EndTime)], out var endTime) ? endTime : null;
-        AccTime = TimeOnly.TryParse(columns[nameof(AccTime)], out var accTime) ? accTime : null;
+        PreTime = TryParseTimeOffset(columns[nameof(PreTime)], out var preTime) ? preTime : null;
+        BeginTime = TryParseTimeOffset(columns[nameof(BeginTime)], out var beginTime) ? beginTime : null;
+        EndTime = TryParseTimeOffset(columns[nameof(EndTime)], out var endTime) ? endTime : null;
+        AccTime = TryParseTimeOffset(columns[nameof(AccTime)], out var accTime) ? accTime : null;
         PreId = long.TryParse(columns[nameof(PreId)], out var preId) ? preId : null;
         BeginId = long.TryParse(columns[nameof(BeginId)], out var beginId) ? beginId : null;
         EndId = long.TryParse(columns[nameof(EndId)], out var endId) ? endId : null;
         AccId = long.TryParse(columns[nameof(AccId)], out var accId) ? accId : null;
+    }
+
+    static bool TryParseTimeOffset(string input, out TimeSpan offset)
+    {
+        if (Regex.Match(input, @"(?<sign>[+-]?)(?<hours>\d+)\:(?<minutes>\d+)") is not { Success: true, Groups: var groups })
+        {
+            offset = default;
+            return false;
+        }
+        var sign = groups["sign"].Value == "-" ? -1 : 1;
+        var hours = int.Parse(groups["hours"].Value);
+        var minutes = int.Parse(groups["minutes"].Value);
+        offset = sign * new TimeSpan(hours, minutes, 0);
+        return true;
     }
 }
 
